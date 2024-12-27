cd4cfe9171a10779ae4ed236dfa8d4edb2e9ffaa
Will Mayer <william.mayer@outlook.com>
2024-12-27 17:00:21 +0400

    cleaned config/application from before---saved

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module WillsPlaylistDownloader
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_record.legacy_connection_handling = false
-    config.active_record.sqlite3_production_warning = false
   end
 end
