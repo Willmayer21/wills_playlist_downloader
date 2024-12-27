d321ddc491d38e0a644a0e96e17d140a5a60d0db
Will Mayer <william.mayer@outlook.com>
2024-12-27 16:46:25 +0400

    Disable database preparation

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module WillsPlaylistDownloader
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.legacy_connection_handling = false
+    config.active_record.sqlite3_production_warning = false
   end
 end
---
+++ lib/tasks/disable_db.rake
@@ -0,0 +1,5 @@
+namespace :db do
+  task :prepare do
+    puts "Database preparation disabled"
+  end
+end
